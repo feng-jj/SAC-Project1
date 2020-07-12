d914ff68e97b5a73d2e85ddd9c96c5354d3235ee
taehwankim3 <taehwan.kim@vanderbilt.edu>
2020-07-11 18:24:02 -0700

    testing branches

--- SACWebApp/SACWebApp/urls.py
+++ SACWebApp/SACWebApp/urls.py
@@ -20,6 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("register/"),
     path('', include('main-page.urls'))
 ]
 

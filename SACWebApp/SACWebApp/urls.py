0e977cab2d4f72e53448d940e82172f1ec414357
taehwankim3 <taehwan.kim@vanderbilt.edu>
2020-07-11 18:38:04 -0700

    linked urls to view

--- SACWebApp/SACWebApp/urls.py
+++ SACWebApp/SACWebApp/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from register import views as v
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("register/"),
+    path("register/", v.register, name="register"),
     path('', include('main-page.urls'))
 ]
 

ed145b900a2a6701adbf016fc15f7a5ac8de0319
taehwankim3 <taehwan.kim@vanderbilt.edu>
2020-07-18 15:51:13 -0700

    added UsersConfig in installed apps

--- SACWebApp/SACWebApp/settings.py
+++ SACWebApp/SACWebApp/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main-page.apps.MainPageConfig',
-    'register.apps.RegisterConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

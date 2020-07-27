1df3add1550cb45cd003618ee7404bb5810ff1ac
Reena Zhang <reena.zhang22@gmail.com>
2020-07-26 23:13:10 -0400

    add crisis line team database

--- SACWebApp/main-page/admin.py
+++ SACWebApp/main-page/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Clinical, Advocacy, MAP, OV, SAFE_Clinic
+from .models import Clinical, Advocacy, MAP, OV, SAFE_Clinic, Crisis_Line
 
 # Register your models here.
 myModels = [Clinical, Advocacy, MAP, OV, SAFE_Clinic]
+myModels += [Crisis_Line]
 admin.site.register(myModels)

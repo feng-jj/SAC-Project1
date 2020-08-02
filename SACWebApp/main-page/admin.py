29eb7d468c37413a54858c25d4d0083fb8059546
Reena Zhang <reena.zhang22@gmail.com>
2020-08-01 22:36:47 -0400

    add all models

--- SACWebApp/main-page/admin.py
+++ SACWebApp/main-page/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Clinical, Advocacy, MAP, OV, SAFE_Clinic, Crisis_Line
+from .models import Clinical, Advocacy, MAP, OV, SAFE_Clinic, Crisis_Line, Prevention, Development, Training
 
 # Register your models here.
 myModels = [Clinical, Advocacy, MAP, OV, SAFE_Clinic]
 myModels += [Crisis_Line]
+myModels += [Prevention]
+myModels += [Development, Training]
 admin.site.register(myModels)
